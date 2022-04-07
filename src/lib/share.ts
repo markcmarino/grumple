ded0630445d2616da1c9e1f557e72cdcc9e78917
markcmarino <markcmarino@gmail.com>
2022-04-06 21:43:41 -0700

    the share tiles are turning

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -77,8 +77,8 @@ const attemptShare = (shareData: object) => {
 
 const getEmojiTiles = (isDarkMode: boolean, isHighContrastMode: boolean) => {
   let tiles: string[] = []
-  tiles.push(isHighContrastMode ? '🟧' : '🟩')
-  tiles.push(isHighContrastMode ? '🟦' : '🟨')
-  tiles.push(isDarkMode ? '⬛' : '⬜')
+  tiles.push(isHighContrastMode ? '🔶' : '🟪')
+  tiles.push(isHighContrastMode ? '🔷' : '🟨')
+  tiles.push(isDarkMode ? '⬛' : '🟥')
   return tiles
 }
