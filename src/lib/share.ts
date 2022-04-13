d317d5c4ddc38ce34c30004b9f303676b55881f2
markcmarino <markcmarino@gmail.com>
2022-04-12 20:32:05 -0700

    betting begins

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -77,8 +77,8 @@ const attemptShare = (shareData: object) => {
 
 const getEmojiTiles = (isDarkMode: boolean, isHighContrastMode: boolean) => {
   let tiles: string[] = []
-  tiles.push(isHighContrastMode ? '👠' : '👠')
-  tiles.push(isHighContrastMode ? '💄' : '💄')
-  tiles.push(isDarkMode ? '👗' : '👗')
+  tiles.push(isHighContrastMode ? '🍒' : '🍒')
+  tiles.push(isHighContrastMode ? '🔔' : '🔔')
+  tiles.push(isDarkMode ? '🍋' : '🍋')
   return tiles
 }
