e4a91e6aec7a7179f6e581aa56f57de15f4335d0
markcmarino <markcmarino@gmail.com>
2022-04-12 20:31:40 -0700

    const getEmojiTiles

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,6 +1,6 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['I\'m Lovin\' it! ', 'The Real Thing', 'Where\'s the Beef?', 'Got Milk?', 'Just Do IT!', 'Taste the Rainbow']
+export const WIN_MESSAGES = ['What happens in Grumple', 'Luck be a Grumple', 'Grumples Eleven', 'Yatzhee', 'Craps!', 'Bingo', 'Jackpot', 'Let it RIDE!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'That\'s all you got?'
 export const WORD_NOT_FOUND_MESSAGE = 'your GRUMPLE is GRUMPLED!'
@@ -17,11 +17,11 @@ export const NOT_CONTAINED_MESSAGE = (letter: string) =>
   `Guess must contain ${letter}`
 export const ENTER_TEXT = 'Enter'
 export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = ['OUTCOME: You win', 'OUTCOME: You lost', 'OUTCOME: Maybe You\'re BORN with It', 'OUTCOME: You Win in error: NO SUCCESS', 'OUTCOME: Your FIT is not LIT', 'OUTCOME: Unfashionable', 'OUTCOME: Distress', 'OUTCOME: Makeover Time!']
-export const GUESS_DISTRIBUTION_TEXT = 'Sephora Cleansing Bars'
-export const NEW_WORD_TEXT = 'Next Vogue Grumple'
+export const STATISTICS_TITLE = ['OUTCOME: You win', 'OUTCOME: You lost', 'OUTCOME: Snake Eyes!', 'OUTCOME: You Win in error: NO SUCCESS', 'OUTCOME: The House ALWAYS wins', 'OUTCOME: You BUSTED', 'OUTCOME: Grumple calls your BLUFF', 'OUTCOME: Cheating suspected']
+export const GUESS_DISTRIBUTION_TEXT = 'Time spent at Buffet'
+export const NEW_WORD_TEXT = 'Time left to BET on Grumple'
 export const SHARE_TEXT = 'Share'
-export const TOTAL_TRIES_TEXT = 'Fashion Fowls'
-export const SUCCESS_RATE_TEXT = 'Hit Fits'
-export const CURRENT_STREAK_TEXT = 'Faux Pas'
-export const BEST_STREAK_TEXT = 'Questionable Taste'
+export const TOTAL_TRIES_TEXT = 'Losses'
+export const SUCCESS_RATE_TEXT = 'Near Hits'
+export const CURRENT_STREAK_TEXT = 'Money Owed'
+export const BEST_STREAK_TEXT = 'Big Tony\'s coming'
